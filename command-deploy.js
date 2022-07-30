4f794f25f44830668c3455e3dc4b4b1b4d455743
ScrambledOmelets <smartsnakie15@gmail.com>
2022-07-30 02:39:15 -0400

    updated so commands can be made in seperate files

--- command-deploy.js
+++ command-deploy.js
@@ -1,4 +1,28 @@
-const { SlashCommandBuilder, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
+
+
+//allows new commands to be added
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
+
+//old
+/*const { SlashCommandBuilder, Routes } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { clientId, guildId, token } = require('./config.json');
 
@@ -9,14 +33,9 @@ const commands = [
 ]
 	.map(command => command.toJSON());
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(token);*/
 
 //deletes all commands
 //rest.put(Routes.applicationCommands(clientId), { body: [] })
 //	.then(() => console.log('Successfully deleted all application commands.'))
 //	.catch(console.error);
-
-//allows new commands to be added
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
