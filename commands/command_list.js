0b0b494ed5bc5bb5720de7536bf0b7ad322eef5b
ScrambledOmelets <smartsnakie15@gmail.com>
2023-10-07 23:29:34 -0400

    new command but doesnt work

---
+++ commands/command_list.js
@@ -0,0 +1,20 @@
+//displays list of commands
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('command_list')
+        .setDescription('sends a list of commans'),
+    async execute(interaction) {
+        await interaction.reply('filler text');
+        const commandEmbed = new EmbedBuilder()
+            .setColor('Blurple')
+            .setTitle('List of Commands')
+            .setDescription(`\/ping - silly ping command (basic bot moment) \n 
+                            \/button - sends a button (does not work currently) \n
+                            \/embed - sends an embed message like this one \n
+                            \/commands - this command`)
+        interaction.channel.send({ embeds: [commandEmbed] });
+    },
+};
