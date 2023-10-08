1de86ce218470a6286b20caf0f7f328bc883fb0d
ScrambledOmelets <smartsnakie15@gmail.com>
2023-10-07 23:44:04 -0400

    added a new command but code so old it dont deploy

---
+++ commands/hello.js
@@ -0,0 +1,22 @@
+//this is the basic setup for all slash commands
+//you would put the code for different actions in the "async execute" function
+//module.exports alows you to export data so you can require() it in other files
+
+const { SlashCommandBuilder } = require('discord.js');
+//if you need access to different things, require() them here at the top
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('hello')
+        .setDescription('replies with hello!'),
+    async execute(interaction) {
+        //code goes here
+        await interaction.reply('Hello!');
+        //await wait(60); //doesnt work, idk why
+        //await interaction.followUp(`If you thought I was going to say "Pong Again!", you're highly mistaken, ${interaction.member.nickname}.`)
+        //await interaction.deleteReply();
+        //await interaction.followUp('Pong again >:)'); //double replies
+    },
+};
+
+//changed from an await in line 14 to a return
